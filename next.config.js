8ed1a57662d5e522ba7788c96b8b56ecd1226ac9
nabezokodaikon <nabezoko.daikon@gmail.com>
2018-12-06 23:38:08 +0900

    Add sourcemap plugins
    The effect is unclear.

--- next.config.js
+++ next.config.js
@@ -1,7 +1,19 @@
 const withPlugins = require('next-compose-plugins')
+const withSourceMaps = require('@zeit/next-source-maps')({
+  devtool: "hidden-source-map"
+})
 const withTypescript = require('@zeit/next-typescript')
 const withCSS = require('@zeit/next-css')
 
-module.exports = withPlugins([withTypescript, withCSS], {
-  cssModules: true
-})
+module.exports = withPlugins([
+  [withSourceMaps, {
+    webpack(config, options) {
+      return config
+    }
+  }],
+  [withTypescript
+  ], 
+  [withCSS, {
+    cssModules: true
+  }]
+])
