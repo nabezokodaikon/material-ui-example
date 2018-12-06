97a63488b4c1fb339a065c55c398647a7a75fe88
nabezokodaikon <nabezoko.daikon@gmail.com>
2018-12-06 21:54:48 +0900

    Clean indent

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import * as React from "react"
 import {
-    Button
+  Button
 } from "@material-ui/core"
 import AccessAlarmIcon from "@material-ui/icons/AccessAlarm"
 import * as style from "../css/style.css"
@@ -8,16 +8,16 @@ import * as style from "../css/style.css"
 interface HelloProps { compiler: string; framework: string; }
 
 export default class Hello extends React.Component<HelloProps, {}> {
-    render() {
-        return (
-            <>
-                <Button variant="contained" color="primary">
-                    Hello Material-UI
-                </Button>
-                <img src="/static/twitter_shiba.png" />
-                <h1 className={style.example}>Hello from and !</h1>
-                <AccessAlarmIcon />
-            </>
-            )
-    }
+  render() {
+    return (
+      <>
+        <Button variant="contained" color="primary">
+          Hello Material-UI
+        </Button>
+        <img src="/static/twitter_shiba.png" />
+        <h1 className={style.example}>Hello from and !</h1>
+        <AccessAlarmIcon />
+      </>
+    )
+  }
 }
