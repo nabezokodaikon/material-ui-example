0b0fb5526c361884cdfe75d1e5850bf9edae61c6
nabezokodaikon <nabezoko.daikon@gmail.com>
2018-12-06 20:28:34 +0900

    Add Marerial-UI Button

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import * as React from "react"
+import Button from "@material-ui/core/Button"
 import * as style from "../css/style.css"
 
 interface HelloProps { compiler: string; framework: string; }
@@ -7,6 +8,9 @@ export default class Hello extends React.Component<HelloProps, {}> {
     render() {
         return (
             <>
+                <Button variant="contained" color="primary">
+                    Hello Material-UI
+                </Button>
                 <img src="/static/twitter_shiba.png" />
                 <h1 className={style.example}>Hello from and !</h1>
             </>
