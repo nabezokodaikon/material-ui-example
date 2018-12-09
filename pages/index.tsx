b48dbcc4344a982dd48471d77d1f71fe06579650
nabezokodaikon <nabezoko.daikon@gmail.com>
2018-12-09 20:43:49 +0900

    Add Header component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import * as React from "react"
+import Header from "../components/Header"
 import {
   Button
 } from "@material-ui/core"
@@ -7,15 +8,17 @@ import Icon from "@material-ui/core/Icon"
 import IconButton from "@material-ui/core/IconButton"
 import SvgIcon from "@material-ui/core/SvgIcon"
 import * as style from "../css/style.css"
-import { createMuiTheme, MuiThemeProvider } from "@material-ui/core";
-import purple from '@material-ui/core/colors/purple';
-import { Theme } from '@material-ui/core/styles/createMuiTheme';
-import { Breakpoint } from '@material-ui/core/styles/createBreakpoints';
-import AppBar from '@material-ui/core/AppBar';
-
-interface HelloProps { compiler: string; framework: string; }
+import { createMuiTheme } from "@material-ui/core";
+// import purple from '@material-ui/core/colors/purple';
+// import { Theme } from '@material-ui/core/styles/createMuiTheme';
+// import { Breakpoint } from '@material-ui/core/styles/createBreakpoints';
+// import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
 
 const theme = createMuiTheme({
+  typography: {
+      useNextVariants: true,
+  },
   palette: {
     primary: {
       light: '#757ce8', // 基本の色よりも明るい色
@@ -32,27 +35,28 @@ const theme = createMuiTheme({
   },
 });
 
-export default class Hello extends React.Component<HelloProps, {}> {
+export default class Hello extends React.Component {
 
   render() {
     return (
       <>
-        <MuiThemeProvider theme={theme}>
-          <AppBar color="secondary">
-            <Button variant="contained" color="primary">
-              Hello Material-UI
-            </Button>
-            <h1 className={style.example}>Hello from and !</h1>
-            <AccessAlarmIcon />
-            <Icon>star</Icon>
-            <IconButton aria-label="Delete">
-            <SvgIcon>
-              <path d="M20 12l-1.41-1.41L13 16.17V4h-2v12.17l-5.58-5.59L4 12l8 8 8-8z" />
-            </SvgIcon>
-            </IconButton>
-          </AppBar>
-        </MuiThemeProvider>
-        <img src="/static/twitter_shiba.png" />
+        <Header />
+        <Toolbar >
+          <Button href="google.com" variant="contained" color="primary">
+            Hello Material-UI
+          </Button>
+          <Button variant="contained" color="primary">
+            Hello Material-UI
+          </Button>
+          <h1 className={style.example}>Hello from and !</h1>
+          <AccessAlarmIcon />
+          <Icon>star</Icon>
+          <IconButton aria-label="Delete">
+          <SvgIcon>
+            <path d="M20 12l-1.41-1.41L13 16.17V4h-2v12.17l-5.58-5.59L4 12l8 8 8-8z" />
+          </SvgIcon>
+          </IconButton>
+        </Toolbar>
       </>
     )
   }
